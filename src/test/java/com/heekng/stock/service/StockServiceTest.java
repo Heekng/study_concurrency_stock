32b917275e0ad083caa88c96549f5aae3d976e76
heekng <rhkd1769@gmail.com>
2022-09-08 16:55:35 +0900

    동시성 이슈가 나타나는 테스트코드 작성

--- src/test/java/com/heekng/stock/service/StockServiceTest.java
+++ src/test/java/com/heekng/stock/service/StockServiceTest.java
@@ -2,15 +2,17 @@ package com.heekng.stock.service;
 
 import com.heekng.stock.domain.Stock;
 import com.heekng.stock.repository.StockRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class StockServiceTest {
@@ -39,6 +41,30 @@ class StockServiceTest {
         // 100 - 1 == 99
         Stock stock = stockRepository.findById(1L).orElseThrow();
         assertThat(stock.getQuantity()).isEqualTo(99);
+    }
+    
+    @Test
+    void 동시에_100개의_요청() throws Exception {
+        int threadCount = 100;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        // 다른 쓰레드에서 작업 수행이 완료될 때 까지 대기할 수 있도록 도우는 클래스
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            executorService.submit(() -> {
+                try {
+                    stockService.decrease(1L, 1L);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        Stock stock = stockRepository.findById(1L).orElseThrow();
+
+        assertThat(stock.getQuantity()).isEqualTo(0L);
     }
 
 }
