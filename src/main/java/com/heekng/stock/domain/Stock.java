7ce5e05e0ecc8ed3d72961bd8abba3babb09f0ac
heekng <rhkd1769@gmail.com>
2022-09-09 15:33:56 +0900

    OptimisticLoc을 이용한 동시성 이슈 해결

--- src/main/java/com/heekng/stock/domain/Stock.java
+++ src/main/java/com/heekng/stock/domain/Stock.java
@@ -4,10 +4,7 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -20,6 +17,9 @@ public class Stock {
     private Long productId;
 
     private Long quantity;
+
+    @Version
+    private Long version;
 
     public Stock(Long productId, Long quantity) {
         this.productId = productId;
